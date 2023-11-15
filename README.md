045634c7ab5a4bbfd7576587f12433a8bd6cf23a
Duljekk <abdulzakisr@gmail.com>
2023-11-15 13:40:06 +0700

    update code

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Jarkom-Modul-3-IT20-2023
+
+Default .bashrc (buat semua)
+
+bash /root/setup.sh
+bash /root/config.sh
---
+++ Switch 4/Stein/config.sh
@@ -0,0 +1,22 @@
+ip a
+# harsunya ip inetnya masih statis -> 192.243.4.2
+
+echo '
+# auto eth0
+# iface eth0 inet static
+#         address 192.243.4.2
+#         netmask 255.255.255.0
+#         gateway 192.243.4.1
+
+
+auto eth0
+iface eth0 inet dhcp' > /etc/network/interfaces
+
+
+# restart node 
+
+
+# TESTING ->
+cat /etc/resolv.conf
+ip a
+#harusnya ke ganti ke range yang ditentuin
